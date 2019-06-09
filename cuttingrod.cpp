61e3a3fe059158765d795fb4e9c1226c9543b03b
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-06-09 11:51:46 +0530

    Create cuttingrod.cpp

---
+++ cuttingrod.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int cutrod(int price[], int n) 
+{ 
+   int val[n+1]; 
+   val[0] = 0; 
+   int i, j; 
+  
+   // Build the table val[] in bottom up manner and return the last entry 
+   // from the table 
+   for (i = 1; i<=n; i++) 
+   { 
+       int max_val = INT_MIN; 
+       for (j = 0; j < i; j++) 
+         max_val = max(max_val, price[j] + val[i-j-1]); 
+       val[i] = max_val; 
+   } 
+  
+   return val[n]; 
+} 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        cout<<cutrod(arr,n)<<endl;
+    }
+}
