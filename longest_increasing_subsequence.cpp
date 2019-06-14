8863450b0cd187e1c56916cd5b865bcc365430eb
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-06-14 12:31:00 +0530

    Update longest_increasing_subsequence.cpp

--- longest_increasing_subsequence.cpp
+++ longest_increasing_subsequence.cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int lis(int arr[],int n){
-    int dp[n];
-    for(int i=0;i<n;i++){
-        dp[i]=1;
-    }
-    for(int i=1;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(arr[i]>arr[j] && dp[i]<dp[j]+1){
-                dp[i]=dp[j]+1;
-            }
-        }
-    }
+   int dp[n];
+   for(int i=0;i<n;i++){
+       dp[i]=1;
+   }
+   for(int i=1;i<n;i++){
+       for(int j=0;j<i;j++){
+           if(arr[j]<arr[i]){
+               dp[i]=max(dp[i],dp[j]+1);
+           }
+       }
+   }
      return *max_element(dp, dp+n);// this function 'max_element' is used to search for the maxi value present in the entire 
 	// array. this function is present in bits/stdc++.h
 }
