8a6c7ad8234f9f25ad5723cb552125f82e6be4ad
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-06-16 10:46:20 +0530

    Update counting_stairs.cpp

--- counting_stairs.cpp
+++ counting_stairs.cpp
@@ -1,44 +1,23 @@
-// our code which is not getting accepted i dont know y.
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void count_stairs(int n){
-    int dp[100000];
+int main(){
+    int t;
+    cin>>t;
+     long long int dp[100000];
     dp[0]=1;
     dp[1]=1;
-    for(int i=2;i<=100000;i++){
+    for(long long int i=2;i<=100000;i++){
         dp[i]=(dp[i-1]%1000000007+dp[i-2]%1000000007)%1000000007;
     }
+    while(t--){
+        int n;
+        cin>>n;
+   
     cout<<dp[n]<<endl;
+    }
+    return 0;
 }
-int main() {
-	int t;
-	cin>>t;
-	while(t--){
-	    int n;
-	    cin>>n;
-	   // cout<<count_stairs(n)<<endl;
-	}
-	return 0;
-}
-
-// efficient solution which is getting accepted and i dont know y??
-#include <iostream>
-#include<math.h>
-using namespace std;
-long long int m=pow(10,9)+7;
-int main() {
-	//code
-	int t;cin>>t;
-	long long int arr[100000];
-	    arr[0]=1;arr[1]=1;
-	    for(int i=2;i<=100000;i++)
-	    {
-	        arr[i]=(arr[i-1]%m+arr[i-2]%m)%m;
-	    }
-	while(t--)
-	{
-	    int n;cin>>n;
-	    cout<<arr[n]<<"\n";
-	}
-	return 0;
-}
+//please note here the solution is actually O(1) u are just 1 time computing
+//all the values from 0 to 100000 and whenever asked u are just fetching
+//the value which is really good so if ur program is giving timeout error u can 
+// u can optimize it like this out of the box thinking.//very efficient solution.
